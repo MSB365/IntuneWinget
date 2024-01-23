632eed8a50da4e9cdba7357eeb66c75330df35d2
Drago Petrovic <drago@msb365.net>
2024-01-23 15:24:24 +0100

    Add files via upload

---
+++ winget-lenovo.ps1
@@ -0,0 +1,12 @@
+# Specify the name of the software you want to check
+$softwareName = "Lenovo.SystemUpdate"
+
+# Check if the software is installed using winget
+$installed = (winget list | Select-String -SimpleMatch $softwareName) -ne $null
+
+# Display the result in green or red
+if ($installed) {
+    winget upgrade -e -h --id $softwareName
+} else {
+    winget install -e -h --id $softwareName
+}
